9905129807a51b8b7b7fc7c6abdd2038f56b085e
Ítalo Alves <italoalvesjp@gmail.com>
2019-09-03 16:03:32 -0300

    basic javascript for canvas setup

---
+++ js/master.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+  let canvas = document.getElementById("game-canvas");
+  let ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "black";
+  ctx.fillRect(0,0, canvas.width, canvas.height);
+}
+
+function game(){
+}
+
+function keyEvent(event){
+
+}
